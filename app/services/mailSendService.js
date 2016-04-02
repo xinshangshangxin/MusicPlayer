f976577185d09803fe76c9599f1c5c71388eabf2
xinshangshangxin <xinshangshangxin@gmail.com>
2016-04-02 09:40:30 +0100

    fix: 发送邮件失败

--- app/services/mailSendService.js
+++ app/services/mailSendService.js
@@ -6,27 +6,7 @@ var Promise = require('bluebird');
 
 // 从环境变量中获取账号
 var getEnvSetting = function() {
-  var _transport = {};
-
-  if(process.env.host) {
-    _transport.host = process.env.host;
-  }
-  if(process.env.port) {
-    _transport.port = process.env.port;
-  }
-  if(process.env.user) {
-    _transport.auth = _transport.auth || {};
-    _transport.auth.user = process.env.user;
-  }
-  if(process.env.pass) {
-    _transport.auth = _transport.auth || {};
-    _transport.auth.pass = process.env.pass;
-  }
-  if(process.env.service) {
-    _transport.service = process.env.service;
-  }
-
-  return _transport;
+  return config.mail || {};
 };
 
 // 默认配置
@@ -40,14 +20,12 @@ var defaultMailOptions = {
 
 var defaultTransport = {
   host: 'smtp.sina.com',
-  port: 25,
+  port: 465,
+  secure: true,
+  tls: {rejectUnauthorized: false},
   auth: {
     user: 'test4code@sina.com',
     pass: 'Test4code;'
-  },
-  secure: false,
-  tls: {
-    rejectUnauthorized: false
   }
 };
 
