276cb63d80a7f90f44deb9f9fe946c802118f36e
xinshangshangxin <xinshangshangxin@gmail.com>
2016-09-14 15:25:31 +0800

    fix: email 配置错误

--- app/services/mailSendService.js
+++ app/services/mailSendService.js
@@ -5,17 +5,26 @@ var nodemailer = require('nodemailer');
 // 默认配置
 var defaultMailOptions = {
   from: 'test4code@sina.com',
-  to: ['codenotification@sina.com'],
+  to: 'codenotification@sina.com',
   subject: 'subject',
   text: 'text',
   html: '<b>Hello world ✔</b>' // html body
 };
 
-var transporter = Promise.promisifyAll(nodemailer.createTransport(config.mailTransport));
+var transporter = nodemailer.createTransport(config.env.mailTransport);
 
-var sendMail = function(mailOtions) {
-  return transporter
-    .sendMailAsync(_.assign({}, defaultMailOptions, mailOtions));
+var sendMail = function(mailOptions) {
+  logger.info('start mailOptions');
+  return new Promise(function(resolve, reject) {
+    transporter.sendMail(_.assign({}, defaultMailOptions, mailOptions), function(error, info) {
+      logger.info('end sendMail');
+      if(error) {
+        reject(error);
+        return;
+      }
+      resolve(info);
+    });
+  });
 };
 
 module.exports = {
